d880a7c80c1cdb7bd0ef3ff2a33e55e92b03d50e
Andrew Spinks <andrewspinks@gmail.com>
2016-09-21 11:44:04 +0900

    Increase timeout as starting the server in vagrant seems sloooooooooooooooooow.

--- src/.mocha.js
+++ src/.mocha.js
@@ -7,7 +7,3 @@ chai.use(sinonChai);
 global.expect = expect;
 global.sinon = sinon;
 
-beforeEach(function(done) {
-    this.timeout(30000); // A very long environment setup.
-    // setTimeout(done, 2500);
-  });
